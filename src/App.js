5bc9963732c7ee3201466174395a14f4f17aaffc
Dimas Saputra <dimas.maulanads@gmail.com>
2023-01-12 22:20:13 +0700

    add more content

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import CompositionSample01 from './embeds/a403/01-composition-sample';
 import HomePage from './pages/HomePage';
 import DeclarativeSample02 from './embeds/a403/02-declarative-sample';
+import ReactCreateElementSample03 from './embeds/a403/03-react-create-element-sample';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Route path="/" element={<HomePage />} />
       <Route path="/a403/01-composition-sample" element={<CompositionSample01 />} />
       <Route path="/a403/02-declarative-sample" element={<DeclarativeSample02 />} />
+      <Route path="/a403/03-react-create-element-sample" element={<ReactCreateElementSample03 />} />
     </Routes>
   );
 }
---
+++ src/embeds/a403/03-react-create-element-sample.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import ReactSandpack from '../../components/sandpacks/ReactSandpack';
+
+const files = {
+  'divContainer.js': {
+    code: `import React from 'react';
+
+const heading = React.createElement('h1', null, 'React');
+const strong = React.createElement('strong', null, 'best tool');
+const paragraph = React.createElement('p', null, ['The ', strong, ' for building UI']);
+const divContainer = React.createElement('div', { className: 'container' }, [heading, paragraph]);
+
+// abaikan kode di bawah ini
+export default divContainer;`,
+    hidden: false,
+    active: true,
+  },
+  'App.js': {
+    code: `import divContainer from './divContainer';
+
+export default function App() {
+  return divContainer;
+}`,
+    hidden: true,
+  },
+};
+function ReactCreateElementSample03() {
+  return (
+    <ReactSandpack
+      files={files}
+      options={{
+        editorWidthPercentage: 75,
+      }}
+    />
+  );
+}
+
+export default ReactCreateElementSample03;
