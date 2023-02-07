aafd7d7b251a88656743f53083624aa794092eaf
Dimas Saputra <dimas.maulanads@gmail.com>
2023-02-07 15:48:53 +0700

    update to typeTriangle

--- src/embeds/a418/01-testing-framework-sample.js
+++ src/embeds/a418/01-testing-framework-sample.js
@@ -2,43 +2,43 @@ import React from 'react';
 import TestSandpack from '../../components/sandpacks/TestSandpack';
 
 const files = {
-  'detectTriangle.js': {
-    code: `const detectTriangle = (sideA, sideB, sideC) => {
-  if (sideA === sideB && sideB === sideC) {
-    return "Segitiga sama sisi";
+  'typeTriangle.js': {
+    code: `function typeTriangle (sideA, sideB, sideC) {
+  if(sideA === sideB && sideB === sideC) {
+    return 'Segitiga sama sisi';
   }
-
-  if (sideA === sideB || sideA === sideC || sideB === sideC) {
-    return "Segitiga sama kaki";
+    
+  if(sideA === sideB || sideA === sideC || sideB === sideC) {
+    return 'Segitiga sama kaki';
   }
-
-  return "Segitiga sembarang";
+    
+  return 'Segitiga sembarang';
 };
 
-module.exports = detectTriangle;
+module.exports = typeTriangle;
 `,
     hidden: false,
   },
-  'detectTriangle.test.js': {
-    code: `const detectTriangle = require("./detectTriangle");
+  'typeTriangle.test.js': {
+    code: `const typeTriangle = require("./typeTriangle");
 
-describe("Sebuah fungsi detectTriangle", () => {
+describe("Sebuah fungsi typeTriangle", () => {
   it("harus mengembalikan segitiga sama sisi ketika semua nilai sisi sama", () => {
-    expect(detectTriangle(4, 4, 4)).toEqual("Segitiga sama sisi");
-    expect(detectTriangle(1, 1, 1)).toEqual("Segitiga sama sisi");
-    expect(detectTriangle(8, 8, 8)).toEqual("Segitiga sama sisi");
+    expect(typeTriangle(4, 4, 4)).toEqual("Segitiga sama sisi");
+    expect(typeTriangle(1, 1, 1)).toEqual("Segitiga sama sisi");
+    expect(typeTriangle(8, 8, 8)).toEqual("Segitiga sama sisi");
   });
 
   it("harus mengembalikan segitiga sama kaki ketika ada dua nilai sisi yang sama", () => {
-    expect(detectTriangle(2, 2, 1)).toEqual("Segitiga sama kaki");
-    expect(detectTriangle(4, 2, 4)).toEqual("Segitiga sama kaki");
-    expect(detectTriangle(1, 3, 3)).toEqual("Segitiga sama kaki");
+    expect(typeTriangle(2, 2, 1)).toEqual("Segitiga sama kaki");
+    expect(typeTriangle(4, 2, 4)).toEqual("Segitiga sama kaki");
+    expect(typeTriangle(1, 3, 3)).toEqual("Segitiga sama kaki");
   });
 
   it("harus mengembalikan segitiga sembarang ketika tidak ada nilai sisi yang sama", () => {
-    expect(detectTriangle(4, 2, 1)).toEqual("Segitiga sembarang");
-    expect(detectTriangle(2, 3, 5)).toEqual("Segitiga sembarang");
-    expect(detectTriangle(8, 2, 1)).toEqual("Segitiga sembarang");
+    expect(typeTriangle(4, 2, 1)).toEqual("Segitiga sembarang");
+    expect(typeTriangle(2, 3, 5)).toEqual("Segitiga sembarang");
+    expect(typeTriangle(8, 2, 1)).toEqual("Segitiga sembarang");
   });
 });`,
     hidden: false,
