25d691da456fe506b3993609bfde5865604c4929
Dimas Saputra <dimas.maulanads@gmail.com>
2023-06-07 17:40:46 +0700

    add 03-bad-textarea.jsx

--- src/embeds/a123/index.jsx
+++ src/embeds/a123/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import MultipleInputElement01 from './01-multiple-input-element';
 import SingleInputElement02 from './02-single-input-element';
+import BadTextarea04 from './03-bad-textarea';
 
 const a123Routes = [
   {
@@ -10,6 +11,10 @@ const a123Routes = [
   {
     path: '/a123/02-single-input-element',
     element: <SingleInputElement02 />,
+  },
+  {
+    path: '/a123/03-bad-textarea',
+    element: <BadTextarea04 />,
   },
 ];
 
