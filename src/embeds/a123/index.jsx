d237eb7302b74b709579724d14c16af0ba6a0e12
Dimas Saputra <dimas.maulanads@gmail.com>
2023-06-07 17:28:44 +0700

    add 01-multiple-input-element.jsx

--- src/embeds/a123/index.jsx
+++ src/embeds/a123/index.jsx
@@ -1,3 +1,11 @@
-const a123Routes = [];
+import React from 'react';
+import MultipleInputElement01 from './01-multiple-input-element';
+
+const a123Routes = [
+  {
+    path: '/a123/01-multiple-input-element',
+    element: <MultipleInputElement01 />,
+  },
+];
 
 export { a123Routes };
