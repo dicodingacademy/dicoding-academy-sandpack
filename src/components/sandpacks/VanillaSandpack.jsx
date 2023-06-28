4cf98c5d19ddd0802c7147888a7a9c511d49477b
Dimas Saputra <dimas.maulanads@gmail.com>
2023-06-28 13:54:32 +0700

    add options props

--- src/components/sandpacks/VanillaSandpack.jsx
+++ src/components/sandpacks/VanillaSandpack.jsx
@@ -2,18 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import BaseSandpack from './base/BaseSandpack';
 
-function VanillaSandpack({ files }) {
+function VanillaSandpack({ files, options }) {
   return (
-    <BaseSandpack template="vanilla" files={files} />
+    <BaseSandpack template="vanilla" files={files} options={options} />
   );
 }
 
 VanillaSandpack.propTypes = {
   files: PropTypes.any,
+  options: PropTypes.any,
 };
 
 VanillaSandpack.defaultProps = {
   files: {},
+  options: {},
 };
 
 export default VanillaSandpack;
