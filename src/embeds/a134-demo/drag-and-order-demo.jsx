b6e3987f7f9afcfd0285b8b843f58b38f3582bfb
Dimas Saputra <dimas.maulanads@gmail.com>
2025-02-20 10:46:27 +0700

    demo for drag and drop

---
+++ src/embeds/a134-demo/drag-and-order-demo.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import DragAndOrder from '../../components/activities/drag-and-order';
+
+function DragAndOrderDemo() {
+  const items = ['DNS Lookup', 'HTTP Request', 'Server Response', 'HTML Parsing', 'CSS Parsing', 'Rendering', 'Painting'];
+
+  return (
+    <div>
+      <DragAndOrder items={items} storageKey="a314-02" />
+    </div>
+  );
+}
+
+export default DragAndOrderDemo;
