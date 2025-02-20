7437d6220cf50d963500e9b7d6eb0f15bc9f5479
Dimas Saputra <dimas.maulanads@gmail.com>
2025-02-20 10:59:41 +0700

    improve style and sample

--- src/embeds/a134-demo/drag-and-order-demo.jsx
+++ src/embeds/a134-demo/drag-and-order-demo.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import DragAndOrder from '../../components/activities/drag-and-order';
 
 function DragAndOrderDemo() {
-  const items = ['DNS Lookup', 'HTTP Request', 'Server Response', 'HTML Parsing', 'CSS Parsing', 'Rendering', 'Painting'];
+  const items = ['DNS Lookup', 'HTTP Request', 'Server Response', 'HTML Parsing', 'CSS Parsing', 'Rendering'];
 
   return (
     <div>
