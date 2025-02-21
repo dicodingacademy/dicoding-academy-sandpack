eafad8506615e2288aaa635902a01b53006398ec
Dimas Saputra <dimas.maulanads@gmail.com>
2025-02-21 13:49:10 +0700

    add hint

--- src/embeds/a134-demo/drag-and-order-demo.jsx
+++ src/embeds/a134-demo/drag-and-order-demo.jsx
@@ -3,10 +3,11 @@ import DragAndOrder from '../../components/activities/drag-and-order';
 
 function DragAndOrderDemo() {
   const items = ['DNS Lookup', 'HTTP Request', 'Server Response', 'HTML Parsing', 'CSS Parsing', 'Rendering'];
+  const hint = 'Urutan yang benar adalah -> DNS Lookup, HTTP Request, Server Response, HTML Parsing, CSS Parsing, Rendering';
 
   return (
     <div>
-      <DragAndOrder items={items} storageKey="a314-01" />
+      <DragAndOrder hint={hint} items={items} storageKey="a314-01" />
     </div>
   );
 }
