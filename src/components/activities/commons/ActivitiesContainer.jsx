d3686a31884baf7219cc6f6184433aeffc9c30dc
nurrizkiadip <nrizki@dicoding.com>
2025-03-13 10:56:13 +0700

    Fix UI container

--- src/components/activities/commons/ActivitiesContainer.jsx
+++ src/components/activities/commons/ActivitiesContainer.jsx
@@ -1,18 +1,18 @@
+import './style.css';
+
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { Sun, Moon } from 'lucide-react';
 
-import './style.css';
-
-function ActivitiesContainer({ children }) {
+export default function ActivitiesContainer({ children, title = 'Komponen Interaktif' }) {
   const [theme, setTheme] = useState(() => {
     const savedTheme = localStorage.getItem('theme');
     return savedTheme || 'dark';
   });
 
   useEffect(() => {
-    document.documentElement.setAttribute('data-theme', theme);
-    localStorage.setItem('theme', theme);
+    document.documentElement.setAttribute('data-theme', theme.toString());
+    localStorage.setItem('theme', theme.toString());
   }, [theme]);
 
   const toggleTheme = () => {
@@ -20,20 +20,32 @@ function ActivitiesContainer({ children }) {
   };
 
   return (
-    <>
-      <div className="activities-container">
+    <article className="activities-container">
+      <header>
+        <div className="activities__header">
+          <div>
+            <h5 className="activities__title">
+              <img src="/assets/learning-activities-icon.png" alt="Learning activities icon" width={20} height={20} />
+              <span>{title}</span>
+            </h5>
+          </div>
+          <div className="activities__buttons">
+            <button type="button" className="activities__buttons-item theme-switch" onClick={() => toggleTheme()}>
+              {theme === 'dark' ? <Sun /> : <Moon />}
+            </button>
+          </div>
+        </div>
+      </header>
+      <hr className="activities__divider" />
+      <main>
         {children}
-      </div>
-      <button type="button" onClick={toggleTheme} className="theme-switch">
-        {theme === 'dark' ? <Sun /> : <Moon />}
-      </button>
-    </>
+      </main>
+    </article>
   );
 }
 
 // proptypes children
 ActivitiesContainer.propTypes = {
   children: PropTypes.node.isRequired,
+  title: PropTypes.string,
 };
-
-export default ActivitiesContainer;
