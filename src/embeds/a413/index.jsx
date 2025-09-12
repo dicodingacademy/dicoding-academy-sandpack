2ea070f4717af6633d3dbbd63a2a701f51b351d8
dimasmds <dimas.maulanads@gmail.com>
2025-09-12 10:31:41 +0700

    add a413 embed

---
+++ src/embeds/a413/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import JoiValidateUser01 from './01-joi-validate-user';
+
+const a413Routes = [
+  {
+    path: '/a413/01-joi-validate-user',
+    element: <JoiValidateUser01 />,
+  },
+];
+
+export { a413Routes };
