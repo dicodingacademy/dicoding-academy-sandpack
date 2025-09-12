2f45e5e29e61a9def0a3c0d027721307bbe1c8a7
dimasmds <dimas.maulanads@gmail.com>
2025-09-12 11:04:37 +0700

    add `03-joi-validate-user-class-component`

--- src/embeds/a413/index.jsx
+++ src/embeds/a413/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import JoiValidateUser01 from './01-joi-validate-user';
 import JoiValidateUserFunctionComponent02 from './02-joi-validate-user-function-component';
+import JoiValidateUserClassComponent03 from './03-joi-validate-user-class-component';
 
 const a413Routes = [
   {
@@ -10,6 +11,10 @@ const a413Routes = [
   {
     path: '/a413/02-joi-validate-user-function-component',
     element: <JoiValidateUserFunctionComponent02 />,
+  },
+  {
+    path: '/a413/03-joi-validate-user-class-component',
+    element: <JoiValidateUserClassComponent03 />,
   },
 ];
 
