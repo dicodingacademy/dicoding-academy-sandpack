655f3c83f2796746f811752edd9bf5e8d872eb8e
Inggih Wicaksono <igihcksn@gmail.com>
2026-03-16 15:16:53 +0700

    fix: remove third parameter from event listener for divs

--- src/embeds/a315/03-event-capturing.jsx
+++ src/embeds/a315/03-event-capturing.jsx
@@ -51,7 +51,7 @@ const divs = Array.from(document.getElementsByTagName('div'));
 divs.forEach((div) => {
   div.addEventListener('click', () => {
     alert(\`ELEMEN \${div.getAttribute('id').toUpperCase()}\`);
-  }, true);
+  });
 });
 `,
   },
